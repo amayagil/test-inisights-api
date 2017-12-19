f77adf9984287b0e4c03af8912096e7da5f36fff
AmayaGil <amaya@redhat.com>
2017-12-19 20:43:51 +0100

    con la clase

--- test-api.py
+++ test-api.py
@@ -13,72 +13,106 @@ URL = "https://access.redhat.com/r/insights"
 # URL for the API to your deployed insights account
 ACCOUNT_API = "%s/v1/account/" % URL
 SYS_API = "%s/v1/groups" % URL
-POST_HEADERS = {'content-type': 'application/json'}
 # Default credentials to login to Insights Account
 USERNAME = "amaya.gil"
 PASSWORD = "ba58o35s"
 SSL_VERIFY = True
+POST_HEADERS = {'content-type': 'application/json'}
 
+class InsightsRequest:
 
-def get_json(location, payload=None):
-    """
-    Performs a GET using the passed URL location
-    """
-    r = requests.get(
-        location,
-        auth=(USERNAME, PASSWORD),
-        verify=SSL_VERIFY,
-        params=payload)
+    def __init__(self, location, data=None):
+        """
+        Class constructor
+        :param location:
+        :param data:
+        """
+        self.location = location
+        self.data = data
 
-    return r.json()
+    def get_insights(self):
+        """
+        Performs a GET using the passed URL location
+        """
+        r = requests.get(
+            self.location,
+            auth=(USERNAME, PASSWORD),
+            verify=SSL_VERIFY,
+            params=self.data)
 
+        if self._check_http_response(r):
+            return r.json()
+        else:
+            return None
 
-def post_json(location, json_data):
-    """
-    Performs a POST and passes the data to the URL location
-    """
+    def post_insights(self):
+        """
+        Performs a POST and passes the data to the URL location
+        """
+        r = requests.post(
+            self.location,
+            data=self.data,
+            auth=(USERNAME, PASSWORD),
+            verify=SSL_VERIFY,
+            headers=POST_HEADERS)
 
-    result = requests.post(
-        location,
-        data=json_data,
-        auth=(USERNAME, PASSWORD),
-        verify=SSL_VERIFY,
-        headers=POST_HEADERS)
+        return self._check_http_response(r)
 
-    print(result.status_code)
-    print(result.reason)
+    def delete_insights(self):
+        """
+            Performs a DELETE and cleans all empty groups
+        """
+        r = requests.delete(
+            self.location,
+            auth=(USERNAME, PASSWORD),
+            verify=SSL_VERIFY)
 
-def clean_empty_groups(id):
+        return self._check_http_response(r)
+
+    def _check_http_response(self, result):
+        if not result.ok:
+            print('HTTP Error found!!! \t' + ' CODE: ' + str(result.status_code) + '\n\t REASON: ' + result.reason)
+        return result.ok
+
+def cute_output(insights_request):
+    
+
+def clean_empty_group(id):
     """
         Performs a DELETE and cleans all empty groups
     """
     location = SYS_API + '/' + str(id)
-    d = requests.delete(
-        location,
-        auth=(USERNAME, PASSWORD),
-        verify=SSL_VERIFY)
+    insights_request = InsightsRequest(location)
 
-    if not d.ok:
-        print('Error group not found!!! \t' + str(d.status_code) + ' ' + d.reason)
+    if not insights_request.delete_insights():
+        print('Unable to clean group ' + str(id))
 
 def create_maint_plan(host_name):
     """
-        Performs a POST and creates a maintenance plan for a give host
+        Performs a POST and creates a maintenance plan for a given host
     """
+    location = URL + '/v2/reports'
+    #location = URL + '/v1/maintenance'
     payload = {'expand': 'system'}
-    result = get_json(URL + '/v2/reports', payload)
+    insights_request = InsightsRequest(location,payload)
     plan = {'name' : 'mi_plan'}
 
-   # for elem in result['resources']:
-    #    if host_name == elem['system']['hostname']:
-            #plan['reports'].append(elem['id'])
-            #plan['add'].append(elem['rule_id'])
-
-    location = URL + '/v1/maintenance'
-    print(location)
-    print(plan)
-    post_json(location, json.dumps(plan))
-    #print(p.json)
+    json_data = insights_request.get_insights()
+    if json_data is not None:
+        for report in json_data['resources']:
+            if host_name == report['system']['hostname']:
+                plan['reports'].append(report['id'])
+                plan['add'].append(report['rule_id'])
+        print(location)
+        print(plan)
+        insights_request.location = URL + '/v1/maintenance'
+        print('cambiando location a ' + location)
+        insights_request.data = json.dumps(plan)
+        result = insights_request.post_insights()
+        if not result:
+            print('Error occurred while create maintenance plan, for host ' + host_name)
+        return result
+    return False
 
 def main():
     """
@@ -87,8 +121,10 @@ def main():
     exist, exit out.
     """
 
+
     payload = {'include': 'systems'}
-    result = get_json(SYS_API, payload)
+    insights_request = InsightsRequest(SYS_API, payload)
+    cute_output(insights_request)
     """print(result)
     print ("Grupos: ")
     for groups in result:
@@ -107,6 +143,6 @@ def main():
     for elem in result['resources']:
         print('Report: ' + str(elem['id']) + ' Rule id: ' + str(elem['rule_id']) + '\tSystem: ' + str(elem['system']['hostname']))
 """
-    create_maint_plan('gherkin')
+    #create_maint_plan('gherkin')
 if __name__ == "__main__":
     main()
